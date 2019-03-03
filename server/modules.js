53bf6eab000c20400b6124ba35a6865bff1c78cd
Barnasree Goswami <barnasree.goswami@cognizant.com>
2019-03-03 21:03:22 +0530

    keep level 1 : node

--- server/modules.js
+++ server/modules.js
@@ -1,9 +1,13 @@
+const connection = require('./db/index');
 /* Replace undefined with Require of your Mongoose connection initialization method */
-const initializeMongooseConnection = undefined;
+const initializeMongooseConnection = () => {
+	connection.connectToMongo();
+	connection.getMongoConnection();	
+};
 /* Replace undefined with Require of your note entity*/
-const noteModel = undefined;
+const noteModel = require('./api/v1/notes/notes.entity');
 /* Replace undefined with Require of your user entity*/
-const userModel = undefined;
+const userModel = require('./api/v1/users/user.entity');
 
 module.exports = {
 	initializeMongooseConnection,
